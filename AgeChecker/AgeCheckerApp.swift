766f87b976ab39343cdc22a192bd5c0b582da9a4
Gustav Persson <mailtogustav7@gmail.com>
2022-01-03 14:37:45 +0100

    Initial Commit

---
+++ AgeChecker/AgeCheckerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AgeCheckerApp.swift
+//  AgeChecker
+//
+//  Created by Gustav Persson on 2022-01-03.
+//
+
+import SwiftUI
+
+@main
+struct AgeCheckerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
