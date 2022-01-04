5a633e3c1eb77206e4e56b2bf86d14f9109ea495
Gustav Persson <mailtogustav7@gmail.com>
2022-01-04 17:31:08 +0100

    Implemented QR Scanner View

--- AgeChecker/ContentView.swift
+++ AgeChecker/ContentView.swift
@@ -6,13 +6,36 @@
 //
 
 import SwiftUI
+import CodeScanner
 
 struct ContentView: View {
     @State var isPresentingScanner = false
+    @State var scannedCode: String = "Skanna en QR-kod för att börja."
+    
+    var scannerSheet : some View {
+        CodeScannerView(
+            codeTypes: [.qr],
+            completion: { result in
+                if case let .success(code) = result {
+                    scannedCode = code.string
+                    isPresentingScanner = false
+                }
+            }
+        )
+    }
     
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(spacing: 10) {
+            Text(scannedCode)
+            
+            Button("Scan QR-code") {
+                isPresentingScanner = true
+            }
+            
+            .sheet(isPresented: $isPresentingScanner) {
+                scannerSheet
+            }
+        }
     }
 }
 
